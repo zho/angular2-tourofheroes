023a02ac88371154273ea3f033a2a6af6457b6ae
zho <joandi85@yahoo.com>
2016-06-22 17:38:37 +0700

    commit

---
+++ app/app.component.ts
@@ -0,0 +1,27 @@
+import {Component} from "@angular/core";
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from "@angular/router-deprecated";
+import {DashboardComponent} from "./dashboard.component";
+import {HeroesComponent} from "./heroes.component";
+import {HeroDetailComponent} from "./hero-detail.component";
+import {HeroService} from "./hero.service";
+
+@Component({
+    selector: 'my-app',
+    templateUrl: '/app/app.component.html',
+    styleUrls: ['app/app.component.css'],
+    directives: [ROUTER_DIRECTIVES],
+    providers: [
+        ROUTER_PROVIDERS,
+        HeroService,
+    ]
+})
+
+@RouteConfig([
+    {path: '/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault: true},
+    {path: '/detail/:id', name: 'HeroDetail', component: HeroDetailComponent},
+    {path: '/heroes', name: 'Heroes', component: HeroesComponent}
+])
+
+export class AppComponent {
+    title = 'Tour of Heroes';
+}
